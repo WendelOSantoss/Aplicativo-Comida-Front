0ff47515f557450e2321e56dcf502a0e42bbc483
Wendel Santos <wendelosantos@hotmail.com>
2023-01-25 19:41:01 -0300

    feat: implementando chakra ui + modificando o requests

---
+++ components/card/homepagecard.tsx
@@ -0,0 +1,3 @@
+interface CardProps {
+  profiles: Profiles;
+}
--- src/types/requests.ts
+++ src/types/requests.ts
@@ -2,3 +2,19 @@ export type LoginRequest = {
   email: string;
   password: string;
 };
+
+
+export type LoginResponse{
+token: string;
+user: User;
+}
+
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+  cpf: string;
+  role: string;
+};
+
+
